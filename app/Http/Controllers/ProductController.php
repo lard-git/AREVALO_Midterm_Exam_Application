5cea346a7fcb800de308bea6ecf6b80b8f5d16bb
lard-git <Elizarjayarevalo@gmail.com>
2026-02-19 21:10:08 +0800

    fix missing ; in productcontroler

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-public function index()
+    public function index()
     {
         $animeProducts = [
             [
@@ -14,17 +14,16 @@ public function index()
                 'title' => 'Violet Evergarden',
                 'genre' => 'Drama/Romance',
                 'description' => 'A young girl seeks to understand what it means to be human.',
-                
             ],
             [
                 'id' => 2,
-                'title' => 'Fullmental Alchemist: Brotherhood',
+                'title' => 'Fullmetal Alchemist: Brotherhood',
                 'genre' => 'Action/Adventure',
                 'description' => 'Two brothers search for the Philosopher\'s Stone to restore their bodies after a failed alchemy experiment.',
             ],
             [
                 'id' => 3,
-                'title' => 'Guren Lagann',
+                'title' => 'Gurren Lagann',
                 'genre' => 'Mecha/Action',
                 'description' => 'In a post-apocalyptic world, a young man and his friends pilot a giant robot to fight against oppressive forces.',
             ],
@@ -39,6 +38,7 @@ public function index()
                 'title' => 'Frieren: Beyond Journeys End',
                 'genre' => 'Fantasy/Adventure',
                 'description' => 'After defeating the Demon King, a group of heroes goes their separate ways. Frieren, an elf mage, embarks on a journey to understand the true meaning of their adventure and the bonds they formed.',
+            ], // <-- ADDED THE MISSING ] HERE
         ];
 
         return view('products.index', compact('animeProducts'));
